a7be98c26f443ba97783d9563253e9a989a17c3b
coderamins <raminsolhi1@gmail.com>
2025-02-17 17:23:03 +0330

    '140311290523'

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')!).render(
+import App from './App'
+var root = document.getElementById('root')
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
